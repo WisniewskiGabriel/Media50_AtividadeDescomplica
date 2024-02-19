6402a61d73d506c595fb56d20f8f76a092b8fa55
Gabriel <gabwisniewski@gmail.com>
2024-02-18 21:45:34 -0300

    Adição da atualização do contador

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,17 @@ import javax.swing.text.MaskFormatter;
 import java.awt.*;
 import java.awt.event.*;
 import java.text.ParseException;
+import java.util.ArrayList;
 
 
 public class Main {
     public static void main(String[] args) throws ParseException {
 
         //Declarar elementos
-        JLabel titleLabel = new JLabel("Calculadora de média de 50 números.");
-        MaskFormatter numberMask = new MaskFormatter("#######");
-        JFormattedTextField fieldTypedNumber = new JFormattedTextField(numberMask);
-        JLabel infoLabel = new JLabel("1/50.");
-        JButton buttonCalculateAction = new JButton("Calcular");
+        JLabel titleLabel                       = new JLabel("Calculadora de média de 50 números.");
+        JTextField fieldTypedNumber    = new JTextField("");
+        JLabel infoLabel                        = new JLabel("1/50.");
+        JButton buttonCalculateAction           = new JButton("Add. próx número");
 
         //Declarar + configurar JFrame
         JFrame mainFrame = new JFrame("Média - Atividade Descomplica");
@@ -23,7 +23,7 @@ public class Main {
 
         //Definições de posição dos elementos
         titleLabel.setBounds            (40, 20,    400, 20);
-        infoLabel.setBounds            (40, 50,    400, 20);
+        infoLabel.setBounds             (40, 50,    400, 20);
         buttonCalculateAction.setBounds (40, 150,   100, 30);
         fieldTypedNumber.setBounds      (40, 80,    200, 30);
 
@@ -36,20 +36,33 @@ public class Main {
         mainFrame.add(buttonCalculateAction);
         mainFrame.add(fieldTypedNumber);
         mainFrame.add(infoLabel);
+
         mainFrame.setVisible(true);
 
+        //Cálculo da média
+        int idx = 0;
+        ArrayList<Integer> inputNumbers = new ArrayList<>();
 
-        //Ações...
         buttonCalculateAction.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                titleLabel.setText("Calculado");
+                Integer typedNumber = Integer.parseInt(fieldTypedNumber.getText());
+                System.out.println(typedNumber);
+                infoLabel.setText(inputNumbers.size()+2+"/50");
+                inputNumbers.add(typedNumber);
+                fieldTypedNumber.setText("");
+                System.out.println(inputNumbers.size());
+                if(inputNumbers.size()>=2){
+                    mainFrame.remove(fieldTypedNumber);
+                    mainFrame.remove(buttonCalculateAction);
+                    mainFrame.revalidate();
+                    mainFrame.repaint();
+
+                }
             }
         });
 
+
+
+
     }
-
-
-
-
-
 }
