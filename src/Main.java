071bb0e87bd065dec805acaae648cd3e4a486d79
Gabriel <gabwisniewski@gmail.com>
2024-02-18 22:12:17 -0300

    Add. checagem de tipo de input (se é inteiro)

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,13 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws ParseException {
 
+        //CONTROLE DE QUANTOS NÚMEROS PODERÃO SER CALCULADOS:
+        int MAX_AMOUNT = 50;
+
         //Declarar elementos
-        JLabel titleLabel                       = new JLabel("Calculadora de média de 50 números.");
+        JLabel titleLabel                       = new JLabel("Calculadora de média de "+MAX_AMOUNT+" números.");
         JTextField fieldTypedNumber             = new JTextField("");
-        JLabel infoLabel                        = new JLabel("1/50.");
+        JLabel infoLabel                        = new JLabel("1/"+MAX_AMOUNT);
         JButton buttonCalculateAction           = new JButton("Add. próx número");
 
         //Declarar + configurar JFrame
@@ -28,7 +31,7 @@ public class Main {
         fieldTypedNumber.setBounds      (40, 80,    200, 30);
 
         //Tamanho do JFrame
-        mainFrame.setSize(400, 300);
+        mainFrame.setSize(600, 300);
 
 
         //Adicionar elementos ao JFrame
@@ -45,36 +48,41 @@ public class Main {
 
         buttonCalculateAction.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                Integer typedNumber = Integer.parseInt(fieldTypedNumber.getText());
+                try {
+                    Integer typedNumber = Integer.parseInt(fieldTypedNumber.getText());
+                    infoLabel.setText(inputNumbers.size()+2+"/"+MAX_AMOUNT);
+                    inputNumbers.add(typedNumber);
+                    fieldTypedNumber.setText("");
 
-                infoLabel.setText(inputNumbers.size()+2+"/50");
-                inputNumbers.add(typedNumber);
-                fieldTypedNumber.setText("");
+                    //Checar se a qtd. máxima foi atingida
+                    if(inputNumbers.size()>=MAX_AMOUNT){
 
-                //Checar se a qtd. máxima foi atingida
-                if(inputNumbers.size()>=50){
+                        //Remover elementos de input
+                        mainFrame.remove(fieldTypedNumber);
+                        mainFrame.remove(buttonCalculateAction);
+                        mainFrame.remove(infoLabel);
 
-                    //Remover elementos de input
-                    mainFrame.remove(fieldTypedNumber);
-                    mainFrame.remove(buttonCalculateAction);
-                    mainFrame.remove(infoLabel);
+                        //Somar e calcular valor da média
 
-                    //Somar e calcular valor da média
+                        Integer finalAverage = 0;
+                        for (Integer number : inputNumbers){
+                            finalAverage = finalAverage + number;
+                        }
+                        finalAverage = finalAverage/inputNumbers.size();
 
-                    Integer finalAverage = 0;
-                    for (Integer number : inputNumbers){
-                        finalAverage = finalAverage + number;
+                        //Exibir resultado
+                        JLabel labelResult = new JLabel("A média entre os "+inputNumbers.size()+" números é "+finalAverage);
+                        labelResult.setBounds(40,120,500,30);
+                        mainFrame.add(labelResult);
+
+                        //Forçar update do frame mudança dos elementos
+                        mainFrame.revalidate();
+                        mainFrame.repaint();
                     }
-                    finalAverage = finalAverage/inputNumbers.size();
-
-                    //Exibir resultado
-                    JLabel labelResult = new JLabel("A média entre os "+inputNumbers.size()+" números é "+finalAverage);
-                    labelResult.setBounds(40,120,200,30);
-                    mainFrame.add(labelResult);
-
-                    //Forçar update do frame mudança dos elementos
-                    mainFrame.revalidate();
-                    mainFrame.repaint();
+                } catch (NumberFormatException | NullPointerException err) {
+                    JOptionPane.showMessageDialog(null, "O input '"+fieldTypedNumber.getText()+
+                            "' não é válido. O tipo esperado: Números inteiros. Ex: 4993");
+                    fieldTypedNumber.setText("");
                 }
             }
         });
