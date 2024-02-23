3a158384190697b4ad89f28547966426a5ec6440
Gabriel <gabwisniewski@gmail.com>
2024-02-23 02:59:15 -0300

    qtd de criterios alterada para 50 novamente

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) throws ParseException {
 
         //CONTROLE DE QUANTOS NÚMEROS PODERÃO SER CALCULADOS:
-        int MAX_AMOUNT = 5;
+        int MAX_AMOUNT = 50;
 
         //Declarar elementos
         JLabel titleLabel                       = new JLabel("Calculadora de média de "+MAX_AMOUNT+" números.");
